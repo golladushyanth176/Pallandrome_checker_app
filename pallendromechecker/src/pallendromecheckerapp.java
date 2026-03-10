fad0d2f5f697280a55caf8530bfad2a478984fc7
Golla-dushyanth02-web <golladushyanth79@gmail.com>
2026-03-10 16:56:02 +0530

    UC13: Performance Comparison

--- pallendromechecker/src/pallendromecheckerapp.java
+++ pallendromechecker/src/pallendromecheckerapp.java
@@ -2,15 +2,25 @@ import java.util.Stack;
 import java.util.Deque;
 import java.util.ArrayDeque;
 
-// Strategy Interface
-interface PalindromeStrategy {
-    boolean checkPalindrome(String word);
-}
+public class UseCase13PalindromeCheckerApp {
 
-// Stack Strategy Implementation
-class StackStrategy implements PalindromeStrategy {
+    // Method 1: Two Pointer Approach
+    public static boolean twoPointerCheck(String word) {
+        int start = 0;
+        int end = word.length() - 1;
 
-    public boolean checkPalindrome(String word) {
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    // Method 2: Stack Based Approach
+    public static boolean stackCheck(String word) {
 
         Stack<Character> stack = new Stack<>();
 
@@ -18,20 +28,17 @@ class StackStrategy implements PalindromeStrategy {
             stack.push(c);
         }
 
-        String reversed = "";
-
-        while (!stack.isEmpty()) {
-            reversed += stack.pop();
+        for (char c : word.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
         }
 
-        return word.equals(reversed);
+        return true;
     }
-}
 
-// Deque Strategy Implementation
-class DequeStrategy implements PalindromeStrategy {
-
-    public boolean checkPalindrome(String word) {
+    // Method 3: Deque Based Approach
+    public static boolean dequeCheck(String word) {
 
         Deque<Character> deque = new ArrayDeque<>();
 
@@ -40,7 +47,6 @@ class DequeStrategy implements PalindromeStrategy {
         }
 
         while (deque.size() > 1) {
-
             if (deque.removeFirst() != deque.removeLast()) {
                 return false;
             }
@@ -48,41 +54,34 @@ class DequeStrategy implements PalindromeStrategy {
 
         return true;
     }
-}
-
-// Context Class
-class PalindromeContext {
-
-    private PalindromeStrategy strategy;
-
-    public PalindromeContext(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-
-    public boolean executeStrategy(String word) {
-        return strategy.checkPalindrome(word);
-    }
-}
-
-// Main Application
-public class UseCase12PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         String word = "madam";
 
-        // Choose strategy dynamically
-        PalindromeStrategy strategy = new StackStrategy();
-        // PalindromeStrategy strategy = new DequeStrategy();
+        // Two Pointer Performance
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerCheck(word);
+        long end1 = System.nanoTime();
 
-        PalindromeContext context = new PalindromeContext(strategy);
+        // Stack Performance
+        long start2 = System.nanoTime();
+        boolean result2 = stackCheck(word);
+        long end2 = System.nanoTime();
 
-        boolean result = context.executeStrategy(word);
+        // Deque Performance
+        long start3 = System.nanoTime();
+        boolean result3 = dequeCheck(word);
+        long end3 = System.nanoTime();
 
-        if (result) {
-            System.out.println("The string \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
-        }
+        // Display Results
+        System.out.println("Two Pointer Result: " + result1);
+        System.out.println("Execution Time: " + (end1 - start1) + " ns");
+
+        System.out.println("\nStack Result: " + result2);
+        System.out.println("Execution Time: " + (end2 - start2) + " ns");
+
+        System.out.println("\nDeque Result: " + result3);
+        System.out.println("Execution Time: " + (end3 - start3) + " ns");
     }
 }
