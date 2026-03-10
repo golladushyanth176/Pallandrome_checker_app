4a10076b8a32927dc8f24d64d70ae3c987356993
Golla-dushyanth02-web <golladushyanth79@gmail.com>
2026-03-10 16:00:55 +0530

    UC6: Queue + Stack Based Palindrome Check

--- pallendromechecker/src/pallendromecheckerapp.java
+++ pallendromechecker/src/pallendromecheckerapp.java
@@ -1,29 +1,41 @@
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class UseCase5PalindromeCheckerApp {
+public class UseCase6PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         // Original string
         String word = "madam";
 
-        // Create a stack
+        // Create Stack and Queue
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
 
-        // Push characters into stack
+        // Push to stack and enqueue to queue
         for (int i = 0; i < word.length(); i++) {
-            stack.push(word.charAt(i));
+            char ch = word.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
         }
 
-        // Build reversed string using pop
-        String reversed = "";
+        boolean isPalindrome = true;
 
+        // Compare dequeue from queue and pop from stack
         while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+
+            char fromStack = stack.pop();
+            char fromQueue = queue.remove();
+
+            if (fromStack != fromQueue) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        // Compare original and reversed string
-        if (word.equals(reversed)) {
+        // Print result
+        if (isPalindrome) {
             System.out.println("The string \"" + word + "\" is a Palindrome.");
         } else {
             System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
